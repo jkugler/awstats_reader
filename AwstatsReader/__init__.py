4b0f8e0de262e9bcdc9749f9d51cc829d23dca29
Joshua Kugler <jkugler@hosanna.kugler.localdomain>
2009-07-20 16:36:26 -0800

    More testing, more code cleanup

--- AwstatsReader/__init__.py
+++ AwstatsReader/__init__.py
@@ -1,26 +1,21 @@
 
-"""Top level object reads the files in the directory.
-
-Ask for object[2009]
-
-Returns list of months it found.
-
-Ask for object[2009][7]
-
-Returns a log file object, from which you can query sections of the stats
-
-
+"""
+AwstatsReader
+A library for querying statistics from AWStats (non-XML) Cache files
 """
 
 import datetime
 import glob
 import os
 
+# Requires odict from http://www.voidspace.org.uk/python/odict.html
 from odict import OrderedDict
 
 def awstats_datetime(date_string):
-    # 20090718035227
-    # 20090701
+    """
+    Parses an AWStats Date/Time or Date string and returns a datetime.datetime
+    or datetime.date object, respectively.
+    """
     if len(date_string) == 14:
         return datetime.datetime(int(date_string[0:4]), int(date_string[4:6]), int(date_string[6:8]),
                                  int(date_string[8:10]), int(date_string[10:12]),int(date_string[12:14]))
@@ -30,7 +25,17 @@ def awstats_datetime(date_string):
     else:
         raise RuntimeError("Invalid date/time string: '%s'" % date_string)
 
+class AttrDict(dict):
+    """
+    Allows dicts to be accessed via dot notation as well as subscripts
+    """
+    def __getattr__(self, name):
+        return self[name]
+
 class AwstatsReader(object):
+    """
+
+    """
     def __init__(self, directory, domain):
         self.__directory = directory
         self.__domain = domain
@@ -112,7 +117,7 @@ class AwstatsMonth(object):
             if line.startswith('END_MAP'):
                 break
 
-    def __get_section(self, name):
+    def __get_raw_section(self, name):
         section_data = OrderedDict()
         k = v = ''
         end_flag = 'END_' + name.upper()
@@ -123,52 +128,62 @@ class AwstatsMonth(object):
             section_data[k] = v.split(' ')
         return section_data
 
+    def __get_section(self, name):
+        if not self.__fobject:
+            self.__init_file()
+        try:
+            if name not in self.__section_cache:
+                self.__section_cache[name] = AwstatsSection(self.__version, name, self.__get_raw_section(name))
+            return self.__section_cache[name]
+        except KeyError:
+            raise KeyError("Section '%s' does not exist" % name)
+
     def __iter__(self):
         if not self.__fobject:
             self.__init_file()
         return (s for s in self.__section_list)
 
-    def __getitem__(self, item):
-        if not self.__fobject:
-            self.__init_file()
-        try:
-            if item not in self.__section_cache:
-                self.__section_cache[item] = AwstatsSection(item, self.__get_section(item))
-            return self.__section_cache[item]
-        except KeyError:
-            raise KeyError("Section '%s' does not exist" % item)
+    __getitem__ = __get_section
+    __getattr__ = __get_section
+
 
     def __str__(self):
         return "<AwstatsMonth " + str(self.__year) + "/" + str(self.__month).rjust(2, '0') +">"
 
 class AwstatsSection(object):
-    def __init__(self, name, raw_data):
-        self.name = name
-        self.format = _section_format['__default__'][name]
-        self.data = raw_data
+    def __init__(self, version, name, raw_data):
+        self.__name = name
+        self.__format = _section_format['__default__'][name]
+        self.__data = raw_data
 
     def __str__(self):
-        return "<AwstatsSection %s, %s>" % (self.name, self.data)
+        return "<AwstatsSection %s, %s>" % (self.__name, self.__data)
 
     def __get_data(self, name):
-        data = self.data[name]
-        if name in self.format:
-            format = self.format[name]
+        data = self.__data[name]
+        if name in self.__format:
+            format = self.__format[name]
         else:
-            format = self.format['__default__']
+            format = self.__format['__default__']
 
-        if len(format) > 1:
+        if isinstance(format, tuple):
             items = {}
             for index, f in enumerate(format):
                 if len(f) == 3 and f[2] == 'opt' and len(data) <= index:
                     break
                 items[f[0]] = f[1](data[index])
-            return items
+            return AttrDict(items)
         else:
-            return format[0](data)
+            return format(data[0])
 
-    def __getitem__(self, name):
-        return self.__get_data(name)
+    __getitem__ = __get_data
+    __getattr__ = __get_data
+
+    def __iter__(self):
+        return (k for k in self.__data.keys())
+
+    def items(self):
+        return ((k,self.__data[k]) for k in self.__data.keys())
 
 _section_format = {}
 
@@ -178,7 +193,7 @@ _section_format['__default__'] = {
         'FirstTime':(('first_time', awstats_datetime)),
         'LastTime':(('last_time',awstats_datetime)),
         'LastUpdate':(('date',awstats_datetime),('parsed',int),('old',int),('new',int),('corrupted',int),('dropped',int)),
-        '__default__':(int),
+        '__default__':int,
         },
     'time':{'__default__':(('pages',int),('hits',int),('bandwidth',int),('not_viewed_pages',int),
                        ('not_viewed_hits',int),('not_viewed_bandwidth',int))},
@@ -191,20 +206,20 @@ _section_format['__default__'] = {
     'worms':{'__default__':(('hits',int),('bandwidth',int),('last_visit',awstats_datetime))},
     'emailsender':{'__default__':(('hits',int),('bandwidth',int),('last_visit',awstats_datetime))},
     'emailreceiver':{'__default__':(('hits',int),('bandwidth',int),('last_visit',awstats_datetime))},
-    'session':{'__default__':(int)},
+    'session':{'__default__':int},
     'sider':{'__default__':(('pages',int),('bandwidth',int),('entry',int),('exit',int))},
     'filetypes':{'__default__':(('hits',int),('bandwidth',int),('bandwidth_without_compression',int),
                                 ('bandwidth_after_compression',int))},
-    'os':{'__default__':(int)},
-    'browser':{'__default__':(int)},
+    'os':{'__default__':int},
+    'browser':{'__default__':int},
     'screensize':{}, # ???????????????
     'unknownreferer':{'__default__':(awstats_datetime)},
     'unknownrefererbrowser':{'__default__':(awstats_datetime)},
     'origin':{'__default__':(('pages',int),('hits',int))},
     'sereferrals':{'__default__':(('pages',int),('hits',int))},
     'pagerefs':{'__default__':(('pages',int),('hits',int))},
-    'searchwords':{'__default__':(int)},
-    'keywords':{'__default__':(int)},
+    'searchwords':{'__default__':int},
+    'keywords':{'__default__':int},
     'misc':{'__default__':(('pages',int),('hits',int),('bandwidth',int))},
     'errors':{'__default__':(('hits',int),('bandwidth',int))},
     'cluster':{'__default__':(('pages',int),('hits',int),('bandwidth',int))},
